5bc403bdd2b267aeee23e8964ecc80a6f76725cc
roulendz <rolands.zeltins@inbox.lv>
2022-03-14 00:47:17 +0200

    Added event Handlers for caching!

--- Plugin.php
+++ Plugin.php
@@ -1,6 +1,12 @@
 <?php namespace Logingrupa\Studybook;
 
+use Event;
 use System\Classes\PluginBase;
+
+//Event
+use Logingrupa\Studybook\Classes\Event\Course\CourseModelHandler;
+use Logingrupa\Studybook\Classes\Event\Reservation\ReservationModelHandler;
+
 
 /**
  * Class Plugin
@@ -10,4 +16,21 @@ class Plugin extends PluginBase
 {
     /** @var array Plugin dependencies */
     public $require = ['Lovata.Toolbox'];
+
+    /**
+     * Plugin boot method
+     */
+    public function boot()
+    {
+        $this->addEventListener();
+    }
+
+    /**
+     * Add event listeners
+     */
+    protected function addEventListener()
+    {
+        Event::subscribe(CourseModelHandler::class);
+        Event::subscribe(ReservationModelHandler::class);
+    }
 }
