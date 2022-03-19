be4d050a06dd2a88cc3f3457df1d63126c673c7e
roulendz <rolands.zeltins@inbox.lv>
2022-03-19 22:51:31 +0200

    Reservations FormHelper for dropdown generation

---
+++ classes/helper/reservation/FormHelper.php
@@ -0,0 +1,54 @@
+<?php namespace Logingrupa\Studybook\Classes\Helper\Reservation;
+
+use Logingrupa\Studybook\Models\AvailableDate;
+use Logingrupa\Studybook\Models\Course;
+
+/**
+ * Class FormHelper
+ * @package Logingrupa\Studybook\Classes\Helper|FormHelper
+ * @author Rolands Zeltins, h1@logingrupa.lv, login grupa|
+ */
+class FormHelper
+{
+
+    /**
+     * Create Course List Dropdown Options
+     * @return array
+     */
+    public static function staticGetActiveCoursesListOptions($model, $formField): array
+    {
+
+        return Course::active()->lists('name', 'id');
+    }
+    /**
+     * Create Course List Dropdown Options
+     * @return array
+     */
+    public static function staticGetSelectedCoursesName($model, $formField): array
+    {
+
+        //Check if valid course ID is selected
+        if (!$model->course_id) {
+            return [];
+        }
+        //        Get Selected Course Available Dates
+        $course = (new \Logingrupa\Studybook\Models\Course)->find($model->course_id);
+        return [$course->name];
+    }
+    /**
+     * Based on Selected Course ID - Create Course Available Date Dropdwon list
+     * @return array
+     */
+    public static function staticGetAvailableDatesListOptions($model, $formField): array
+    {
+        //Check if valid course ID is selected
+        if (!$model->course_id) {
+            return [];
+        }
+        //        Get Selected Course Available Dates
+        $availabledates = (new \Logingrupa\Studybook\Models\Course)->find($model->course_id)->availabledates();
+        //        Create Available Dare List Dropdown Options
+        return $availabledates->orderBy('datetime', 'DESC')->lists('datetime', 'datetime');
+    }
+
+}
