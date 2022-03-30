b6b81914493de51bedef4811c6c93efb234d5ace
roulendz <rolands.zeltins@inbox.lv>
2022-03-31 00:18:56 +0300

    Added custom column type - Price

--- Plugin.php
+++ Plugin.php
@@ -5,7 +5,9 @@ use System\Classes\PluginBase;
 
 //Event
 use Logingrupa\Studybook\Classes\Event\Course\CourseModelHandler;
+use Logingrupa\Studybook\Classes\Event\Category\CategoryModelHandler;
 use Logingrupa\Studybook\Classes\Event\Reservation\ReservationModelHandler;
+use Logingrupa\Studybook\Classes\Event\AvailableDate\AvailableDateModelHandler;
 
 
 /**
@@ -26,11 +28,61 @@ class Plugin extends PluginBase
     }
 
     /**
+     * Register columnTypes
+     *
+     */
+    public function registerListColumnTypes()
+    {
+        return [
+            'price' => [$this, 'evalPriceListColumn'],
+            'old_price' => [$this, 'evalOldPriceListColumn'],
+            'phone_number' => [$this, 'evalPhoneNumberListColumn'],
+        ];
+    }
+
+    /**
+     * Currency Price Column Type
+     */
+    public function evalPriceListColumn($value, $column, $record)
+    {
+        if (is_numeric($value)) {
+            if ($value > 0) {
+                return '€' . number_format($value / 100, 2, '.', ' ');
+            }
+            return 'BEZMAKSAS';
+        }
+        return $value;
+    }
+    /**
+     * Currency Old Price Column Type
+     */
+    public function evalOldPriceListColumn($value, $column, $record)
+    {
+        if (is_numeric($value)) {
+            if ($value > 0) {
+                return '<s>'. '€' . number_format($value / 100, 2, '.', ' '). '<s>';
+            }
+            return 'BEZMAKSAS';
+        }
+        return $value;
+    }
+    A
+    /**
+     * Currency Old Price Column Type
+     */
+    public function evalPhoneNumberListColumn($value, $column, $record)
+    {
+        return '<a href="tel:'. $value .'" >'. $value . '</a>';
+    }
+
+    /**
      * Add event listeners
      */
     protected function addEventListener()
     {
         Event::subscribe(CourseModelHandler::class);
         Event::subscribe(ReservationModelHandler::class);
+        Event::subscribe(CategoryModelHandler::class);
+        Event::subscribe(AvailableDateModelHandler::class);
     }
 }
