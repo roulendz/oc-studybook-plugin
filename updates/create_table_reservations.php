98223863c4be1da09bb4d096e3d7c8449a2016f0
roulendz <rolands.zeltins@inbox.lv>
2022-03-31 00:26:10 +0300

    Added indexes to DB

--- updates/create_table_reservations.php
+++ updates/create_table_reservations.php
@@ -25,21 +25,22 @@ class CreateTableReservations extends Migration
         {
             $obTable->engine = 'InnoDB';
             $obTable->increments('id')->unsigned();
-            $obTable->string('status')->nullable()->index();
-            $obTable->string('name')->index();
+            $obTable->string('status')->nullable();
+            $obTable->string('name');
             $obTable->integer('course_id')->unsigned()->nullable();
             $obTable->foreign('course_id')->references('id')->on('logingrupa_studybook_courses');
             $obTable->boolean('active')->default(0);
-            $obTable->dateTime('start_at')->default(null)->index();
-            $obTable->string('full_name')->nullable()->index();
+            $obTable->dateTime('start_at')->default(null);
+            $obTable->foreign('start_at')->references('datetime')->on('logingrupa_studybook_availabledates');
+            $obTable->string('full_name')->nullable();
             $obTable->integer('student_id')->unsigned()->nullable();
             $obTable->foreign('student_id')->references('id')->on('users');
             $obTable->boolean('attendance')->nullable()->default(0);
             $obTable->boolean('health')->nullable()->default(0);
-            $obTable->string('phone')->nullable()->index();
-            $obTable->string('email')->nullable()->index();
-            $obTable->string('model_status')->nullable()->index();
-            $obTable->string('slug')->unique()->index();
+            $obTable->string('phone')->nullable();
+            $obTable->string('email')->nullable();
+            $obTable->string('model_status')->nullable();
+            $obTable->string('slug')->unique();
             $obTable->integer('price')->nullable();
             $obTable->integer('old_price')->nullable();
             $obTable->string('code')->nullable();
@@ -47,6 +48,17 @@ class CreateTableReservations extends Migration
             $obTable->text('preview_text')->nullable();
             $obTable->text('description')->nullable();
             $obTable->timestamps();
+
+            $obTable->index('name');
+            $obTable->index('course_id');
+            $obTable->index('start_at');
+            $obTable->index('full_name');
+            $obTable->index('student_id');
+            $obTable->index('phone');
+            $obTable->index('email');
+            $obTable->index('status');
+            $obTable->index('price');
+            $obTable->index('old_price');
         });
     }
 
