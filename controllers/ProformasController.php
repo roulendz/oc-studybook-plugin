4f0b655e6f8b76f6ca8d096899c6b1bbe699da04
roulendz <rolands.zeltins@inbox.lv>
2022-05-11 22:03:44 +0300

    Proforma view for frontend

--- controllers/ProformasController.php
+++ controllers/ProformasController.php
@@ -2,6 +2,7 @@
 
 
 use Backend\Classes\Controller;
+use Illuminate\Support\Facades\DB;
 use Logingrupa\Studybook\Models\Transaction;
 use View;
 
@@ -12,7 +13,10 @@ use View;
 class ProformasController extends Controller
 {
     public function view($slug){
-        $transaction = Transaction::whereSlug($slug)->with(['student', 'reservation', 'reservation.course', 'parent', 'parent.children'])->get();
+        $transaction = Transaction::whereSlug($slug)
+            ->with(['student', 'reservation', 'reservation.course', 'parent.children', 'children'])->get();
+//        $debitSum = Transaction::whereSlug($slug)->parent()->children()->sum('debit');
+//        dd($transaction);
         return View::make('logingrupa.studybook::frontend.proforma', ['transaction' => $transaction]);
     }
 }
